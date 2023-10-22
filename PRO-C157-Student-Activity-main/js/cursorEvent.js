28c18968225634443669a9cbf37ebeb9ee7aae40
swetaprangya <105152167+swetaprangya@users.noreply.github.com>
2023-10-22 15:54:20 +0530

    Add files via upload

---
+++ PRO-C157-Student-Activity-main/js/cursorEvent.js
@@ -0,0 +1,48 @@
+AFRAME.registerComponent("cursor-listener", {
+    schema: {
+      selectedItemId: { default: "", type: "string" },
+    },
+    init: function () {
+      this.handleMouseEnterEvents();
+      this.handleMouseLeaveEvents();
+    },
+    handlePlacesListState: function () {
+      const id = this.el.getAttribute("id");
+      const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+      if (placesId.includes(id)) {
+        const placeContainer = document.querySelector("#places-container");
+        placeContainer.setAttribute("cursor-listener", {
+          selectedItemId: id,
+        });
+        this.el.setAttribute("material", {
+          color: "#D76B30",
+          opacity: 1,
+        });
+      }
+    },
+    handleMouseEnterEvents:function(){
+        this.el.addEventListener("mouseenter",()=>{
+            this.handlePlacesListState()
+        })
+    },
+    handleMouseLeaveEvents:function(){
+        this.el.addEventListener("mouseleave",()=>{
+            const{selectedItemId}=this.data 
+            if(selectedItemId){
+                const el=document.querySelector(`#${selectedItemId}`)
+                const id=el.getAttribute("id")
+                if(id==selectedItemId){
+                    el.setAttribute("meterial",{
+                        color:"yellow",
+                        opacity:1,
+                    })
+                }
+            }
+        })
+    }
+              
+          }
+        
+      );
+    
+  
